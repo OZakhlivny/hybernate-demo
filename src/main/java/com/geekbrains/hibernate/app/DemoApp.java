a9eda27ccbc23b84309a16f471fbeb61849e97aa
OZakhlivny <DeadE7ern17y>
2020-09-19 14:06:09 +0200

    Initial commit

---
+++ src/main/java/com/geekbrains/hibernate/app/DemoApp.java
@@ -0,0 +1,13 @@
+package com.geekbrains.hibernate.app;
+
+import com.geekbrains.hibernate.app.data.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.*;
+
+public class DemoApp {
+    public static void main(String[] args) {
+    }
+}
